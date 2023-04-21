f7c52c514c75c1edbed9d90487c5b9f4284ca2f0
kabu <kabukunz@gmail.com>
2023-04-21 10:59:12 +0200

    more of it

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-./bin/run_examples 9
-python3 scripts/plot_mesh.py '/Users/max/Developer/Stage/Workspace/3D/TQMesh/auxiliary/example_data/Example_9.txt'
+./bin/run_examples $1
+python3 scripts/plot_mesh.py /Users/max/Developer/Stage/Workspace/3D/TQMesh/auxiliary/example_data/Example_$1.txt
